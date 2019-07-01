cf7dfc50fd5ea1f8828c9d1922327681df31cbab
user <alexey.dudchenko@gmail.com>
2019-07-01 16:41:18 +0200

    title added

--- kata.sh
+++ kata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo 'Title goes here' > file.txt
+
 echo '===============' >> file.txt
 echo '' >> file.txt
 git add file.txt
@@ -9,7 +10,9 @@ git commit -m 'Title placeholder'
 echo 'These are:' >> file.txt
 echo ' * red' >> file.txt
 echo ' * blue' >> file.txt
+echo ' * green' >> file.txt
+
 git add file.txt
 git commit -m 'My colors'
 
-echo ' * green' >> file.txt
+
